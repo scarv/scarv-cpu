c043e9cbd565bb8c8e8b4ca9ac4662bc69a0a9d4
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-07-26 16:16:53 +0100

    Add infrastructure for fairness assumptions in RVFI wrapper.
     On branch dev/rvfi
     Changes to be committed:
    	modified:   verif/rvfi/rvfi_wrapper.sv
    
     Changes not staged for commit:
    	modified:   external/riscv-formal (modified content, untracked content)
    	modified:   rtl/core/frv_lsu.v
    	modified:   rtl/core/frv_pipeline_back.v
    	modified:   rtl/core/frv_pipeline_dispatch.v
    	modified:   rtl/core/frv_pipeline_execute.v
    	modified:   rtl/core/frv_pipeline_writeback.v

--- verif/rvfi/rvfi_wrapper.sv
+++ verif/rvfi/rvfi_wrapper.sv
@@ -44,7 +44,146 @@ parameter XL = 31;
 (*keep*) `rvformal_rand_reg         dmem_error; // Error
 (*keep*) `rvformal_rand_reg [XL:0]  dmem_rdata; // Read data
 
-frv_core i_dut(
+//
+// Memory request/response counters
+// --------------------------------------------------------------------
+
+reg [31:0] imem_reqs;
+reg [31:0] imem_rsps;
+        
+wire [31:0] n_imem_reqs = imem_reqs + (imem_req  && imem_gnt);
+wire [31:0] n_imem_rsps = imem_rsps + (imem_recv && imem_ack);
+
+reg [31:0] dmem_reqs;
+reg [31:0] dmem_rsps;
+
+wire [31:0] n_dmem_reqs = dmem_reqs + (dmem_req  && dmem_gnt);
+wire [31:0] n_dmem_rsps = dmem_rsps + (dmem_recv && dmem_ack);
+
+reg  [31:0] imem_outstanding;
+wire [31:0] n_imem_outstanding = imem_outstanding + (imem_reqs > imem_rsps);
+
+reg  [31:0] dmem_outstanding;
+wire [31:0] n_dmem_outstanding = dmem_outstanding + (dmem_reqs > dmem_rsps);
+
+// Instruction memory request/response counter.
+always @(posedge clock) begin
+    if(reset) begin
+        imem_reqs <= 0;
+        imem_rsps <= 0;
+    end else begin
+        imem_reqs <= imem_reqs + (imem_req  && imem_gnt);
+        imem_rsps <= imem_rsps + (imem_recv && imem_ack);
+    end
+end
+
+// Time for which there is >0 outstanding instruction memory requests.
+always @(posedge clock) begin
+    if(reset || imem_reqs <= imem_rsps) begin
+        imem_outstanding <= 0;
+    end else begin
+        imem_outstanding <= n_imem_outstanding;
+    end
+end
+
+// Time for which there is >0 outstanding data memory requests.
+always @(posedge clock) begin
+    if(reset || dmem_reqs <= dmem_rsps) begin
+        dmem_outstanding <= 0;
+    end else begin
+        dmem_outstanding <= n_dmem_outstanding;
+    end
+end
+
+// Data memory request/response counter.
+always @(posedge clock) begin
+    if(reset) begin
+        dmem_reqs <= 0;
+        dmem_rsps <= 0;
+    end else begin
+        dmem_reqs <= dmem_reqs + (dmem_req  && dmem_gnt);
+        dmem_rsps <= dmem_rsps + (dmem_recv && dmem_ack);
+    end
+end
+
+// Instruction memory request grant stall counter.
+reg  [4:0] imem_req_gnt_stall;
+wire [4:0] n_imem_req_gnt_stall = imem_req_gnt_stall + (imem_req&&!imem_gnt);
+
+always @(posedge clock) begin
+    if(reset || imem_gnt) begin
+        imem_req_gnt_stall <= 0;
+    end else begin
+        imem_req_gnt_stall <= n_imem_req_gnt_stall;
+    end
+end
+
+
+// Data memory request grant stall counter.
+reg  [4:0] dmem_req_gnt_stall;
+wire [4:0] n_dmem_req_gnt_stall = dmem_req_gnt_stall + (dmem_req&&!dmem_gnt);
+
+
+always @(posedge clock) begin
+    if(reset || dmem_gnt) begin
+        dmem_req_gnt_stall <= 0;
+    end else begin
+        dmem_req_gnt_stall <= n_dmem_req_gnt_stall;
+    end
+end
+
+
+//
+// Fairness restrictions
+// --------------------------------------------------------------------
+
+//
+// Don't allow memory *requests* to be ignored for more than N cycles.
+always @(posedge clock) begin
+    restrict(imem_req_gnt_stall < 3);
+    restrict(dmem_req_gnt_stall < 3);
+end
+
+//
+// Don't allow memory requests to "back up" without being acknowledged.
+// Maximum of N cycles spent with outstanding requests.
+always @(posedge clock) begin
+    restrict(imem_outstanding <= 3);
+    restrict(dmem_outstanding <= 3);
+end
+
+//
+// Fairness restrictions so we never get a memory response we
+// didn't issue a request for.
+always @(posedge clock) begin
+    restrict(dmem_rsps <= dmem_reqs);
+    restrict(imem_rsps <= imem_reqs);
+
+    // If num responses >= num requests, *mem_recv is clear. Else True.
+    restrict(dmem_rsps >= dmem_reqs ? dmem_recv == 1'b0 : 1'b1);
+    restrict(imem_rsps >= imem_reqs ? imem_recv == 1'b0 : 1'b1);
+end
+
+//
+// RVFI can't handle propagation of bus errors properly, so assume
+// they don't happen. *gulp*.
+always @(posedge clock) begin
+    if(imem_recv) begin
+        assume(imem_error == 1'b0);
+    end
+    if(dmem_recv) begin
+        assume(dmem_error == 1'b0);
+    end
+end
+
+
+//
+// DUT instance
+// --------------------------------------------------------------------
+
+frv_core #(
+.TRACE_INSTR_WORD(1'b1) // Require tracing of instruction words.
+) i_dut(
 .g_clk          (clock          ), // global clock
 .g_resetn       (g_resetn       ), // synchronous reset
 .rvfi_valid     (rvfi_valid     ),
