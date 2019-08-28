e09011ff29c34411a69a8fc5635d2683e8c54a84
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-08-28 11:23:29 +0100

    Add unit (ish) test for making sure instrucitons run.
    - Simply ad-hoc way of making sure instructions actually run
      to completion. More for debugging than testing.
    
     On branch dev/xc_malu-integration
     Changes to be committed:
    	modified:   verif/unit/Makefile.in
    	new file:   verif/unit/instructions/Makefile.in
    	new file:   verif/unit/instructions/instructions.c
    
     Changes not staged for commit:
    	modified:   external/riscv-formal (untracked content)
    	modified:   external/xcrypto-rtl (modified content)
    	modified:   flow/riscv-formal/rvfi-checks.cfg
    	modified:   flow/xcfi-formal/config.sby.template
    	modified:   verif/rvfi/xcfi_macros.sv
    
     Untracked files:
    	verif/rvfi/models/clmul.v
    	verif/rvfi/models/xc_madd_3.v
    	verif/rvfi/xcfi_shared.vh

---
+++ verif/unit/instructions/instructions.c
@@ -0,0 +1,22 @@
+
+#include "unit_test.h"
+
+int test_main() {
+
+    uint32_t rs1 = 0xF0F0F0F0;
+    uint32_t rs2 = 0x0F0F0F0F;
+    uint32_t rd;
+
+    __asm__ ("clmul %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2));
+
+    if(rd) {
+
+        return 0;
+
+    } else {
+        
+        return 1;
+        
+    }
+}
+
