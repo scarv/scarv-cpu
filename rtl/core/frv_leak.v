ffa2c3637e7380fc8452aedf17a786ee9f0997f8
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-10-02 10:46:04 +0100

    Add random/zeroing leakage fence parameter.
    - Sets uarch resources to zero or pseudo random
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   rtl/core/frv_leak.v

--- rtl/core/frv_leak.v
+++ rtl/core/frv_leak.v
@@ -47,21 +47,29 @@ generate if(XC_CLASS_LEAK) begin // Leakage instructions are implemented
         end
     end
 
-    wire n_prng_lsb = leak_prng[31] ~^
-                      leak_prng[21] ~^
-                      leak_prng[ 1] ~^
-                      leak_prng[ 0]  ;
-    
-    wire [XL:0] n_prng = {leak_prng[XL-1:0], n_prng_lsb};
+    if(XC_CLASS_LEAK_STRONG) begin
 
-    //
-    // Process for updating the LFSR.
-    always @(posedge g_clk) begin
-        if(!g_resetn) begin
-            leak_prng <= PRNG_RESET_VALUE;
-        end else if(leak_fence) begin
-            leak_prng <= n_prng;
+        wire n_prng_lsb = leak_prng[31] ~^
+                          leak_prng[21] ~^
+                          leak_prng[ 1] ~^
+                          leak_prng[ 0]  ;
+        
+        wire [XL:0] n_prng = {leak_prng[XL-1:0], n_prng_lsb};
+
+        //
+        // Process for updating the LFSR.
+        always @(posedge g_clk) begin
+            if(!g_resetn) begin
+                leak_prng <= PRNG_RESET_VALUE;
+            end else if(leak_fence) begin
+                leak_prng <= n_prng;
+            end
         end
+
+    end else begin
+        
+        always @(*) leak_prng = 0;
+
     end
 
 end else begin // Leakage instructions are not implemented
