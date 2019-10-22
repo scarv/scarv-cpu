scarv/scarv-cpu

commit b8c9ab7d273f455797e654318eb91c8fec18c5da

Fetch buffer improvments.
- Increase size of fetch buffer to 64-bits.
- Will enable faster/more efficient fetching of instructions
  in the future.

 On branch master
 Your branch is up-to-date with 'origin/master'.

 Changes to be committed:
	modified:   rtl/core/frv_core_fetch_buffer.v
	modified:   rtl/core/frv_pipeline_fetch.v

rtl/core/frv_core_fetch_buffer.v
@@ -18,7 +18,7 @@ input              f_err        , // Input error
 input  [XL:0]      f_in         , // Input data
 output             f_ready      , // Buffer can accept more bytes.
 
-output  [1:0]      buf_depth    , // Current buffer depth
+output  [2:0]      buf_depth    , // Current buffer depth
 output             buf_16       , // 16 bit instruction next to be output
 output             buf_32       , // 32 bit instruction next to be output
 output [XL:0]      buf_out      , // Output data
@@ -33,131 +33,75 @@ input              buf_ready      // Eat 2/4 bytes
 // Common core parameters and constants
 `include "frv_common.vh"
 
-reg [16:0] buffer    [2:0];
-reg [16:0] n_buffer  [2:0];
-reg [ 1:0] bdepth         ;
-reg [ 1:0] n_bdepth       ;
+reg  [63:0] buffer  ;
+wire [63:0] n_buffer;
 
-assign f_ready = bdepth  < 2'd2                     ||
-                 bdepth == 2'd2 && (eat_2 || eat_4) ||
-                 bdepth == 2'd3 && (         eat_4) ;
+reg  [ 3:0] buffer_err;
+wire [ 3:0] n_buffer_err;
 
-assign buf_out   = {buffer[1][15:0], buffer[0][15:0]};
-assign buf_err   =  buffer[0][16  ];
+reg  [ 2:0] bdepth  ;
+wire [ 2:0] n_bdepth;
+
+// Is the buffer ready for new data to be inserted.
+assign f_ready   = bdepth <= 2                      ||
+                   bdepth == 3  && (eat_2 || eat_4) ||
+                   bdepth == 4  && (         eat_4)  ;
+
+
+assign buf_out   =  buffer    [31:0];
+assign buf_err   = |buffer_err[ 1:0];
 
 assign buf_16    = buf_out[1:0] != 2'b11 && |bdepth;
-assign buf_32    = buf_out[1:0] == 2'b11;
+assign buf_32    = buf_out[1:0] == 2'b11 && |bdepth;
 assign buf_depth = bdepth;
 
-assign buf_out_2 = bdepth >= 2'd1 && buf_16;
-assign buf_out_4 = bdepth >= 2'd2 && buf_32;
+assign buf_out_2 = bdepth >= 3'd1 && buf_16;
+assign buf_out_4 = bdepth >= 3'd2 && buf_32;
 
+// 2 byte instruction being removed from the buffer.
 wire   eat_2     = buf_out_2 && buf_ready;
+
+// 4 byte instruction being removed from the buffer.
 wire   eat_4     = buf_out_4 && buf_ready;
 
 assign buf_valid = buf_out_2 || buf_out_4 ;
 
-always @(*) begin
-    // Stay the same by default
-    n_bdepth      = bdepth   ;
-    n_buffer[0]   = buffer[0];
-    n_buffer[1]   = buffer[1];
-    n_buffer[2]   = buffer[2];
-    
-    case(bdepth)
-    0 : begin // empty
-        if(f_4byte) begin
-            n_buffer[0] = {f_err,f_in[15: 0]};
-            n_buffer[1] = {f_err,f_in[31:16]};
-            n_bdepth    = 2;
-            //`FRV_COVER(1);
-        end else if(f_2byte) begin
-            n_buffer[0] = {f_err,f_in[31:16]};
-            n_bdepth    = 1;
-            //`FRV_COVER(1);
-        end
-    end
-    1 : begin // 1 halfword
-        if(f_4byte) begin
-            if(eat_2) begin// Load 4, eat 2
-                n_buffer[0] = {f_err,f_in[15: 0]};
-                n_buffer[1] = {f_err,f_in[31:16]};
-                n_bdepth    = 2;
-                //`FRV_COVER(1);
-            end else begin          // Load 4
-                n_buffer[1] = {f_err,f_in[15: 0]};
-                n_buffer[2] = {f_err,f_in[31:16]};
-                n_bdepth    = 3;
-                //`FRV_COVER(1);
-            end
-        end else begin              // Nothing being added
-            if(eat_2) begin// eat 2
-                n_bdepth    = 0;
-                //`FRV_COVER(1);
-            end
-        end
-    end
-    2 : begin // 2 halfwords
-        if(f_4byte) begin
-            if(eat_4) begin         // Load 4, eat 4
-                n_buffer[0] = {f_err,f_in[15: 0]};
-                n_buffer[1] = {f_err,f_in[31:16]};
-                n_bdepth    = 2;
-                //`FRV_COVER(1);
-            end else if(eat_2) begin// Load 4, eat 2
-                n_buffer[0] = buffer[1];
-                n_buffer[1] = {f_err,f_in[15: 0]};
-                n_buffer[2] = {f_err,f_in[31:16]};
-                n_bdepth    = 3;
-                //`FRV_COVER(1);
-            end
-        end else begin              // Nothing being added
-            if(eat_4) begin         // eat 4
-                n_bdepth    = 0;
-                //`FRV_COVER(1);
-            end else if(eat_2) begin// eat 2
-                n_buffer[0] = buffer[1];
-                n_bdepth    = 1;
-                //`FRV_COVER(1);
-            end
-        end
-    end
-    3 : begin // 3 halfwords
-        if(f_4byte) begin
-            if(eat_4) begin         // Load 4, eat 4
-                n_buffer[0] = buffer[2];
-                n_buffer[1] = {f_err,f_in[15: 0]};
-                n_buffer[2] = {f_err,f_in[31:16]};
-                n_bdepth    = 3;
-                //`FRV_COVER(1);
-            end
-        end else begin              // Nothing being added
-            if(eat_4) begin         // eat 4
-                n_buffer[0] = buffer[2];
-                n_bdepth    = 1;
-                //`FRV_COVER(1);
-            end else if(eat_2) begin// eat 2
-                n_buffer[0] = buffer[1];
-                n_buffer[1] = buffer[2];
-                n_bdepth    = 2;
-                //`FRV_COVER(1);
-            end
-        end
-    end
-    endcase
-end
+// Amount of stuff being added to the buffer this cycle.
+wire [ 2:0] bdepth_add = {1'b0, f_4byte, f_2byte};
 
+// Amount of stuff being removed from the buffer this cycle.
+wire [ 2:0] bdepth_sub = {1'b0, eat_4  , eat_2  };
+
+// Where to insert new data into the buffer.
+wire [ 2:0] insert_at  = bdepth - bdepth_sub;
+
+// Buffer depth in the next cycle.
+assign      n_bdepth   = bdepth + bdepth_add - bdepth_sub;
+
+wire [31:0] n_buffer_d      = f_2byte ? {16'b0, f_in[31:16]} :
+                                                f_in         ;
+
+wire [63:0] n_buffer_or_in  = {32'b0,n_buffer_d} << (16*insert_at );
+wire [63:0] n_buffer_shf_out= buffer             >> (16*bdepth_sub);
+
+assign      n_buffer        = n_buffer_or_in | n_buffer_shf_out;
+
+wire [ 3:0] n_err_or_in     = {2'b0, {2{f_err}}} << insert_at;
+wire [ 3:0] n_err_shf_out   = buffer_err         >> insert_at;
+
+assign      n_buffer_err    = n_err_or_in    | n_err_shf_out;
+
+//
+// Register updates.
 always @(posedge g_clk) begin
     if(!g_resetn || flush) begin
-        bdepth      <= 0;
-        buffer[0]   <= 0;
-        buffer[1]   <= 0;
-        buffer[2]   <= 0;
+        buffer      <= 64'b0;
+        buffer_err  <=  4'b0;
+        bdepth      <=  3'd0;
     end else begin
-        bdepth      <= n_bdepth   ;
-        buffer[0]   <= n_buffer[0];
-        buffer[1]   <= n_buffer[1];
-        buffer[2]   <= n_buffer[2];
+        buffer      <= n_buffer    ;
+        buffer_err  <= n_buffer_err;
+        bdepth      <= n_bdepth    ;
     end
 end
 

rtl/core/frv_pipeline_fetch.v
@@ -62,7 +62,7 @@ wire f_2byte;   // Buffer should store 2 bytes of input.
 
 wire       buf_16;
 wire       buf_32;
-wire [1:0] buf_depth; // Current buffer depth.
+wire [2:0] buf_depth; // Current buffer depth.
 
 wire buf_out_2 ; // Buffer has entire valid 2 byte instruction.
 wire buf_out_4 ; // Buffer has entire valid 4 byte instruction.
@@ -103,8 +103,8 @@ wire incomplete_instr = buf_32 && buf_depth == 1;
 // Don't start a memory fetch request if there are already a bunch of
 // outstanding, unrecieved responses.
 wire        n_imem_req  =
-    ((f_ready || cf_change) && reqs_outstanding<=FRV_MAX_REQS_OUTSTANDING &&
-     (buf_depth <= 2 || incomplete_instr)) ||
+    ((f_ready || cf_change) && n_reqs_outstanding<=FRV_MAX_REQS_OUTSTANDING ||
+    incomplete_instr) ||
     (imem_req && !imem_gnt);
 
 //

commit 2b89cd416ef57081ad8d124c50b67a0c206446a4

Fix bug in the fetch buffer introduced in b8c9ab7
- Original change allowed bus error responses to enter the fetch buffer
  even when the response bus was not being sampled.
- The fix make sure that a new error condition can only enter the buffer
  on a valid sample, *and*, that the buffer registers are only clocked
  iff data is entering/leaving the buffer.

 On branch master
 Your branch is up-to-date with 'origin/master'.

 Changes to be committed:
	modified:   rtl/core/frv_core_fetch_buffer.v

rtl/core/frv_core_fetch_buffer.v
@@ -87,10 +87,14 @@ wire [63:0] n_buffer_shf_out= buffer             >> (16*bdepth_sub);
 
 assign      n_buffer        = n_buffer_or_in | n_buffer_shf_out;
 
-wire [ 3:0] n_err_or_in     = {2'b0, {2{f_err}}} << insert_at;
-wire [ 3:0] n_err_shf_out   = buffer_err         >> insert_at;
+wire        n_err_in        = f_err && (f_2byte || f_4byte);
+wire [ 3:0] n_err_or_in     = {2'b0, {2{n_err_in}}} << insert_at;
+wire [ 3:0] n_err_shf_out   = buffer_err            >> insert_at;
 
 assign      n_buffer_err    = n_err_or_in    | n_err_shf_out;
+
+wire        update_buffer   = f_4byte || f_2byte || buf_ready;
+
 
 //
 // Register updates.
@@ -99,7 +103,7 @@ always @(posedge g_clk) begin
         buffer      <= 64'b0;
         buffer_err  <=  4'b0;
         bdepth      <=  3'd0;
-    end else begin
+    end else if(update_buffer) begin
         buffer      <= n_buffer    ;
         buffer_err  <= n_buffer_err;
         bdepth      <= n_bdepth    ;