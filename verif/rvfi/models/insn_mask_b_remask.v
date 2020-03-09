493606457d7c83557be3f14fe9a38e297484b627
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-03-09 15:31:42 +0000

    Masking ISE: Add remaining boolean masking model checkers.
     On branch scarv/xcrypto/masking-ise
     Changes to be committed:
    	new file:   verif/rvfi/models/insn_mask_b_and.v
    	new file:   verif/rvfi/models/insn_mask_b_ior.v
    	new file:   verif/rvfi/models/insn_mask_b_not.v
    	new file:   verif/rvfi/models/insn_mask_b_remask.v
    	new file:   verif/rvfi/models/insn_mask_b_xor.v

---
+++ verif/rvfi/models/insn_mask_b_remask.v
@@ -0,0 +1,35 @@
+
+`include "xcfi_macros.sv"
+
+module xcfi_insn_spec (
+
+    `XCFI_TRACE_INPUTS,
+
+    `XCFI_SPEC_OUTPUTS
+
+);
+
+`XCFI_INSN_CHECK_COMMON
+
+wire [XL:0] result_s1 = `RS1 ^ `RS1_HI ^ rvfi_mask_data;
+wire [XL:0] result_s0 =                  rvfi_mask_data;
+
+assign spec_valid       = rvfi_valid && dec_mask_b_not;
+assign spec_trap        = 1'b0   ;
+assign spec_rs1_addr    = `FIELD_RS1_ADDR;
+assign spec_rs2_addr    = 0;
+assign spec_rs3_addr    = 0;
+assign spec_rd_addr     = `FIELD_RD_ADDR;
+assign spec_rd_wdata    = `FIELD_RD_ADDR ? result_s0 : 32'b0;
+assign spec_rd_wide     = 1'b1;
+assign spec_rd_wdatahi  = result_s1;
+assign spec_pc_wdata    = rvfi_pc_rdata + 4;
+assign spec_mem_addr    = 0;
+assign spec_mem_rmask   = 0;
+assign spec_mem_wmask   = 0;
+assign spec_mem_wdata   = 0;
+
+endmodule
+
+
+
