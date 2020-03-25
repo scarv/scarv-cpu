b3215ba11fed5d9e619151851bd7601d48d3c4eb
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-03-25 15:50:14 +0000

    Script to re-run all AES variants.

--- bin/verilator-build-aes-variants.sh
+++ bin/verilator-build-aes-variants.sh
@@ -7,8 +7,13 @@ set -x
 # 1. Variant name
 # 2. XC_AES_VARIANT parameter number = {1,2,3,4}
 function build_variant {
-make VL_DIR=$FRV_WORK/verilator-aes-$1 \
-     VL_VERILOG_PARAMETERS=-GXC_AES_VARIANT=$2
+    make VL_DIR=$FRV_WORK/verilator-aes-$1 \
+         VL_VERILOG_PARAMETERS=-GXC_AES_VARIANT=$2
+    
+    make build-unit-aes-${1} run-unit-aes-${1} \
+        VL_DIR=$FRV_HOME/work/verilator-aes-${1} \
+        VL_VERILOG_PARAMETERS=-GXC_AES_VARIANT=$2 \
+        UNIT_TIMEOUT=200000
 }
 
 build_variant   v1  1
