ea52a5078d0748219a14ace8aa5e224b7eb08619
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-04-30 12:14:07 +0100

    Masking ISE: rename and fix mask add/sub model checkers
     On branch scarv/xcrypto/masking-ise
     Your branch is up-to-date with 'origin/scarv/xcrypto/masking-ise'.
    
     Changes to be committed:
    	renamed:    verif/rvfi/models/insn_mask_a_add.v -> verif/rvfi/models/insn_mask_b_add.v
    	renamed:    verif/rvfi/models/insn_mask_a_sub.v -> verif/rvfi/models/insn_mask_b_sub.v

--- verif/rvfi/models/insn_mask_b_sub.v
+++ verif/rvfi/models/insn_mask_b_sub.v
@@ -13,23 +13,20 @@ module xcfi_insn_spec (
 
 `XCFI_INSN_CHECK_COMMON
 
-wire [XL:0] u_rs1   = `XCFI_UNMASK_A(`RS1, `RS1_HI);
-wire [XL:0] u_rs2   = `XCFI_UNMASK_A(`RS2, `RS2_HI);
-wire [XL:0] u_result= u_rs1 + u_rs2;
-wire [XL:0] u_rd    = `XCFI_UNMASK_A(`RD , `RD_HI );
+wire [XL:0] u_rs1   = `XCFI_UNMASK_B(`RS1, `RS1_HI);
+wire [XL:0] u_rs2   = `XCFI_UNMASK_B(`RS2, `RS2_HI);
+wire [XL:0] u_result= u_rs1 - u_rs2;
+wire [XL:0] u_rd    = `XCFI_UNMASK_B(`RD , `RD_HI );
 
 `XCFI_SPEC_CHECK_BEGIN
 
     assume(`FIELD_RD_ADDR != 0);
 
-    assume(u_rs1 == 1);
-    assume(u_rs2 == 1);
-
     assert(u_rd == u_result);
 
 `XCFI_SPEC_CHECK_END
 
-assign spec_valid       = rvfi_valid && dec_mask_b_add;
+assign spec_valid       = rvfi_valid && dec_mask_b_sub;
 assign spec_trap        = 1'b0   ;
 assign spec_rs1_addr    = `FIELD_RS1_ADDR;
 assign spec_rs2_addr    = 0;
@@ -45,4 +42,5 @@ assign spec_mem_wmask   = 0;
 assign spec_mem_wdata   = 0;
 
 endmodule
+
 
