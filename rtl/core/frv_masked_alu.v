263ccdcf5399dc4dd89e637038da0057df6b7294
phthinh <hung3@e.ntu.edu.sg>
2020-05-07 12:11:22 +0100

    add shift/rotate instructions for the masked ALU

--- rtl/core/frv_masked_alu.v
+++ rtl/core/frv_masked_alu.v
@@ -82,27 +82,7 @@ parameter MASKING_ISE_TI      = 1'b1;
 // Use a fast implementaiton of the masking ISE instructions.
 parameter MASKING_ISE_FAST    = 1'b1;
 
-wire [XL:0] gs_0;
-wire [XL:0] mxor0, mxor1;
-wire [XL:0] mand0, mand1;
-wire [XL:0] madd0, madd1;
-
-wire [ 2:0] seq_cnt;
-wire        addsub_ena;
-wire 	    mlogic_ena;
-
-// decode 
-wire donot = op_b_not;
-wire doxor = op_b_xor;
-wire doand = op_b_and;
-wire doior = op_b_ior;
-wire doadd = op_b_add;
-wire dosub = op_b_sub;
-
-wire dob2a = op_b2a;
-wire doa2b = op_a2b;
-
-// prng for masking, mask refreshing
+// PRNG for masking, mask refreshing
 wire          prng_req;
 reg  [31:0]   prng;
 wire        n_prng_lsb = prng[31] ~^ prng[21] ~^ prng[ 1] ~^ prng[ 0];
@@ -113,18 +93,39 @@ always @(posedge g_clk) begin
     else if(prng_req || prng_update)  prng <= n_prng;
 end
 assign prng_req = ready;
-//boolean masking ior and subtract by controlling the complement of the operands.
+
+wire [XL:0] gs_0;
+wire [XL:0] mxor0, mxor1;
+wire [XL:0] mand0, mand1;
+wire [XL:0] madd0, madd1;
+
+wire [ 2:0] seq_cnt;
+wire        addsub_ena;
+wire 	    mlogic_ena;
+
+// Decode 
+wire donot = op_b_not;
+wire doxor = op_b_xor;
+wire doand = op_b_and;
+wire doior = op_b_ior;
+wire doadd = op_b_add;
+wire dosub = op_b_sub;
+
+wire dob2a = op_b2a;
+wire doa2b = op_a2b;
+
+// Boolean masked IOR and SUB by controlling the complement of the operands.
 wire [XL:0] s_a1; // signed(rs2_s1)
 wire [XL:0] s_b1; // signed(rs1_s1)
 assign s_a1 = (doior      )? ~rs1_s1: rs1_s1;
 assign s_b1 = (doior|dosub)? ~rs2_s1: rs2_s1;
 
-//boolean masking not by masking second operand of xor
+// Boolean masked NOT by masking second operand of XOR
 wire [XL:0] n_b0, n_b1;
 assign n_b0 = (donot)? {XLEN{1'b0}}: rs2_s0;
 assign n_b1 = (donot)? {XLEN{1'b0}}: s_b1;
 
-//boolean mask to arithmetic mask by reusing the boolean masking add/sub
+// Boolean masking to arithmetic masking by reusing the boolean masked add/sub
 //a0^a1 = s0-s1
 //(a^x ^ x) + (b^y ^y) = (a+b)^z ^ z
 //=>
@@ -133,7 +134,7 @@ assign n_b1 = (donot)? {XLEN{1'b0}}: s_b1;
 wire [XL:0] b2a_b0;
 assign      b2a_b0 = prng; 
 
-//arithmetic mask to boolean mask by reusing the boolean masking add/sub
+// Arithmetic masking to boolean masking by reusing the boolean masked add/sub
 //a0-a1 = s0^s1
 //(a^x ^ x) - (b^y ^y) = (a-b)^z ^ z
 //=>
@@ -142,7 +143,7 @@ assign      b2a_b0 = prng;
 wire [XL:0] a2b_b0;
 assign      a2b_b0 = ~rs1_s1; 
 
-//boolean masking logic 
+// Boolean masked logic 
 wire [XL:0] op_a0, op_a1, op_b0, op_b1;
 assign op_a0 = rs1_s0;
 assign op_a1 = (dob2a)? rs1_s1 : (doa2b)? {XLEN{1'b0}}: s_a1;
@@ -168,7 +169,7 @@ msklogic_ins(
     .o_gs(      gs_0),
     .rdy(       mlogic_rdy));
 
-//boolean masking add/sub
+// Boolean masked add/sub
 wire        madd_rdy;
 mskaddsub   
 #(  .MASKING_ISE_TI(MASKING_ISE_TI))
@@ -187,7 +188,7 @@ mskaddsub_ins(
     .o_s1(      madd1), 
     .rdy(       madd_rdy));
 
-//Control unit for boolean masking calculations
+// Control unit for Boolean masked calculations
 wire        dologic  = (~flush) & (donot | doxor | doand | doior);
 wire        doaddsub = (~flush) & (doadd | dosub | dob2a | doa2b);
 mskalu_ctl  mskaluctl_ins(
@@ -202,8 +203,38 @@ mskalu_ctl  mskaluctl_ins(
     .mlogic_ena(mlogic_ena), 
     .addsub_ena(addsub_ena));
 
-// boolean mask, umask, remask
-wire opmask = (~flush) & (op_b_mask   | op_a_mask  );   //masking operand
+// Boolean masked shift/rotate
+
+wire dosrl   = (~flush) & op_b_srli;
+wire dosll   = (~flush) & op_b_slli;
+wire doror   = (~flush) & op_b_rori;
+
+wire doshr   = dosrl | dosll | doror;
+wire shr_rdy = doshr;
+
+wire [XL:0]  mshr0, mshr1;
+shfrot shfrpt_ins0(
+    .s(      rs1_s0     ), 
+    .shamt(  rs2_s0[4:0]), // Shift amount 
+    .rp (    prng       ), // random padding
+    .srli(   dosrl      ), // Shift  right
+    .slli(   dosll      ), // Shift  left
+    .rori(   doror      ), // Rotate right
+    .r(      mshr0      )  
+);
+
+shfrot shfrpt_ins1(
+    .s(      rs1_s1     ), 
+    .shamt(  rs2_s0[4:0]), // Shift amount 
+    .rp (    prng       ), // random padding
+    .srli(   dosrl      ), // Shift  right
+    .slli(   dosll      ), // Shift  left
+    .rori(   doror      ), // Rotate right
+    .r(      mshr1      )  
+);
+
+// Boolean masking and remasking
+wire opmask = (~flush) & (op_b_mask   | op_a_mask  );   //masking operation
 wire remask = (~flush) & (op_b_remask | op_a_remask);
 wire b_mask = (~flush) & (op_b_mask   | op_b_remask);
 
@@ -259,6 +290,7 @@ assign rd_s0 = {XLEN{donot  }} &  mxor0 |
                {XLEN{dosub  }} &  madd0 |
                {XLEN{doa2b  }} &  madd0 |
                {XLEN{dob2a  }} &  32'b0 | // FIXME (madd0^madd1) |
+               {XLEN{doshr  }} &  mshr0 |
                {XLEN{msk_rdy}} &  rmask0;
 
 assign rd_s1 = {XLEN{donot  }} & ~mxor1 |
@@ -269,9 +301,10 @@ assign rd_s1 = {XLEN{donot  }} & ~mxor1 |
                {XLEN{dosub  }} &  madd1 |
                {XLEN{doa2b  }} &  madd1 |
                {XLEN{dob2a  }} &  prng  |
+               {XLEN{doshr  }} &  mshr1 |
                {XLEN{msk_rdy}} &  rmask1;
 
-assign ready = (dologic && mlogic_rdy) | madd_rdy | msk_rdy ;
+assign ready = (dologic && mlogic_rdy) | madd_rdy | msk_rdy | shr_rdy;
 assign mask  = prng;
 
 endmodule
@@ -312,6 +345,99 @@ always @(posedge g_clk) begin
 end
 assign    mlogic_ena =  dologic_valid  && (ctl_state == S_IDL);
 assign    addsub_ena =  doaddsub_valid && ((mlogic_rdy && (ctl_state == S_IDL)) | (ctl_state != S_IDL));
+
+endmodule
+
+// Shift/rotate module to operate on each sharing with random pading
+module shfrot(
+input  [31:0] s    , // input share
+input  [ 4:0] shamt, // Shift amount 
+input  [31:0] rp   , // random padding
+input         srli , // Shift  right
+input         slli , // Shift  left
+input         rori , // Rotate right
+
+output [31:0] r      // ouput share
+);
+
+wire left = slli;
+wire right= srli | rori;
+
+wire [31:0] l0 =  s;
+
+wire [31:0]  l1;
+wire [31:0]  l2;
+wire [31:0]  l4;
+wire [31:0]  l8;
+wire [31:0] l16;
+
+wire         l1_rpr = (rori)? l0[   0] : rp[    0];
+wire [ 1:0]  l2_rpr = (rori)? l1[ 1:0] : rp[ 2: 1];
+wire [ 3:0]  l4_rpr = (rori)? l2[ 3:0] : rp[ 6: 3];
+wire [ 7:0]  l8_rpr = (rori)? l4[ 7:0] : rp[14: 7];
+wire [15:0] l16_rpr = (rori)? l8[15:0] : rp[30:15];
+//
+// Level 1 code - shift/rotate by 1.
+wire [31:0] l1_left  = {l0[30:0], rp[31]};
+wire [31:0] l1_right = {l1_rpr  , l0[31:1]};
+  
+wire l1_l  = left  && shamt[0];
+wire l1_r  = right && shamt[0];
+wire l1_n  =         !shamt[0];
+
+assign l1  = {32{l1_l}} & l1_left  |
+             {32{l1_r}} & l1_right |
+             {32{l1_n}} & l0       ;
+
+// Level 2 code - shift/rotate by 2..
+wire [31:0] l2_left  = {l1[29:0], rp[30:29]};
+wire [31:0] l2_right = {l2_rpr  , l1[31: 2]};
+  
+wire l2_l  = left  && shamt[1];
+wire l2_r  = right && shamt[1];
+wire l2_n  =         !shamt[1];
+
+assign l2  = {32{l2_l}} & l2_left  |
+             {32{l2_r}} & l2_right |
+             {32{l2_n}} & l1       ;
+
+// Level 3 code - shift/rotate by 4.
+wire [31:0] l4_left  = {l2[27:0], rp[28:25]};
+wire [31:0] l4_right = {l4_rpr  , l2[31: 4]};
+  
+wire l4_l  = left  && shamt[2];
+wire l4_r  = right && shamt[2];
+wire l4_n  =         !shamt[2];
+assign l4  = {32{l4_l}} & l4_left  |
+             {32{l4_r}} & l4_right |
+             {32{l4_n}} & l2       ;
+
+// Level 4 code - shift/rotate by 8.
+wire [31:0] l8_left  = {l4[23:0], rp[24:17]};
+wire [31:0] l8_right = {l8_rpr  , l4[31: 8]};
+  
+wire l8_l  = left  && shamt[3];
+wire l8_r  = right && shamt[3];
+wire l8_n  =         !shamt[3];
+
+assign l8  = {32{l8_l}} & l8_left  |
+             {32{l8_r}} & l8_right |
+             {32{l8_n}} & l4       ;
+
+// Level 5 code - shift/rotate by 16.
+wire [31:0] l16_left  = {l8[15:0], rp[16: 1]};
+wire [31:0] l16_right = {l16_rpr , l8[31:16]};
+  
+wire l16_l  = left  && shamt[4];
+wire l16_r  = right && shamt[4];
+wire l16_n  =         !shamt[4];
+
+assign l16  = {32{l16_l}} & l16_left  |
+              {32{l16_r}} & l16_right |
+              {32{l16_n}} & l8        ;
+
+// output
+assign r = l16;
 
 endmodule
 
