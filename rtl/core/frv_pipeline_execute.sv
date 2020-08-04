scarv/scarv-cpu

commit aa92755e86563208992c5ee069a4309bca3b9565

skw: rtl: Fixes for saes32 integration.
- See #30.

- Instruction values didn't progress down the pipe completely or get
  written back.

- Pass BS immediate argument without having to clock s2_opr_c

 On branch scarv/skywater/dev
 Your branch is ahead of 'origin/scarv/skywater/dev' by 1 commit.
   (use "git push" to publish your local commits)

 Changes to be committed:
	modified:   rtl/core/frv_common.svh
	modified:   rtl/core/frv_pipeline_decode.sv
	modified:   rtl/core/frv_pipeline_decode.svh
	modified:   rtl/core/frv_pipeline_execute.sv
	modified:   rtl/core/frv_pipeline_memory.sv
	modified:   rtl/core/frv_pipeline_writeback.sv

 Changes not staged for commit:
	modified:   external/embench-iot (modified content, untracked content)
	modified:   external/riscv-formal (untracked content)

rtl/core/frv_pipeline_execute.sv
@@ -174,7 +174,7 @@ wire        cfu_cond_taken =
 
 wire        cfu_always_take= cfu_jalr || cfu_jali || cfu_jalr;
 
-wire [4:0]  n_s3_uop_cfu   =
+wire [OP:0]  n_s3_uop_cfu   =
     cfu_cond        ? (cfu_cond_taken ? CFU_TAKEN : CFU_NOT_TAKEN)  :
     cfu_always_take ? s2_uop                                        :
                       s2_uop                                        ;
@@ -202,28 +202,29 @@ localparam LOGIC_GATING    = 1; // Gate sub-module inputs to save toggling
 wire        cry_valid      = fu_cry         ; // Inputs valid.
 wire [XL:0] cry_rs1        = s2_opr_a       ; // Source register 1
 wire [XL:0] cry_rs2        = s2_opr_b       ; // Source register 2
-wire [ 3:0] cry_imm        = s2_opr_c[3:0]  ; // enc_rcon for aes32/64.
+wire [ 3:0] cry_bs         = {2'b00,s2_uop[OP:OP-1]}; // bs for saes32.
+wire [OP:0] cry_uop        = {2'b00, s2_uop[OP-2:0]};
 
-wire        cry_op_lut4lo        = fu_cry && s2_uop == CRY_LUT4LO       ;
-wire        cry_op_lut4hi        = fu_cry && s2_uop == CRY_LUT4HI       ;
-wire        cry_op_saes32_encs   = fu_cry && s2_uop == CRY_SAES32_ENCS  ;
-wire        cry_op_saes32_encsm  = fu_cry && s2_uop == CRY_SAES32_ENCSM ;
-wire        cry_op_saes32_decs   = fu_cry && s2_uop == CRY_SAES32_DECS  ;
-wire        cry_op_saes32_decsm  = fu_cry && s2_uop == CRY_SAES32_DECSM ;
-wire        cry_op_ssha256_sig0  = fu_cry && s2_uop == CRY_SSM3_P0      ;
-wire        cry_op_ssha256_sig1  = fu_cry && s2_uop == CRY_SSM3_P1      ;
-wire        cry_op_ssha256_sum0  = fu_cry && s2_uop == CRY_SSM4_KS      ;
-wire        cry_op_ssha256_sum1  = fu_cry && s2_uop == CRY_SSM4_ED      ;
-wire        cry_op_ssha512_sum0r = fu_cry && s2_uop == CRY_SSHA256_SIG0 ;
-wire        cry_op_ssha512_sum1r = fu_cry && s2_uop == CRY_SSHA256_SIG1 ;
-wire        cry_op_ssha512_sig0l = fu_cry && s2_uop == CRY_SSHA256_SUM0 ;
-wire        cry_op_ssha512_sig0h = fu_cry && s2_uop == CRY_SSHA256_SUM1 ;
-wire        cry_op_ssha512_sig1l = fu_cry && s2_uop == CRY_SSHA512_SUM0R;
-wire        cry_op_ssha512_sig1h = fu_cry && s2_uop == CRY_SSHA512_SUM1R;
-wire        cry_op_ssm3_p0       = fu_cry && s2_uop == CRY_SSHA512_SIG0L;
-wire        cry_op_ssm3_p1       = fu_cry && s2_uop == CRY_SSHA512_SIG0H;
-wire        cry_op_ssm4_ks       = fu_cry && s2_uop == CRY_SSHA512_SIG1L;
-wire        cry_op_ssm4_ed       = fu_cry && s2_uop == CRY_SSHA512_SIG1H;
+wire        cry_op_lut4lo        = 1'b0                                 ;
+wire        cry_op_lut4hi        = 1'b0                                 ;
+wire        cry_op_saes32_encs   = fu_cry && cry_uop == CRY_SAES32_ENCS  ;
+wire        cry_op_saes32_encsm  = fu_cry && cry_uop == CRY_SAES32_ENCSM ;
+wire        cry_op_saes32_decs   = fu_cry && cry_uop == CRY_SAES32_DECS  ;
+wire        cry_op_saes32_decsm  = fu_cry && cry_uop == CRY_SAES32_DECSM ;
+wire        cry_op_ssm4_ks       = fu_cry && cry_uop == CRY_SSM4_KS      ;
+wire        cry_op_ssm4_ed       = fu_cry && cry_uop == CRY_SSM4_ED      ;
+wire        cry_op_ssha256_sig0  = fu_cry && s2_uop == CRY_SSHA256_SIG0 ;
+wire        cry_op_ssha256_sig1  = fu_cry && s2_uop == CRY_SSHA256_SIG1 ;
+wire        cry_op_ssha256_sum0  = fu_cry && s2_uop == CRY_SSHA256_SUM0 ;
+wire        cry_op_ssha256_sum1  = fu_cry && s2_uop == CRY_SSHA256_SUM1 ;
+wire        cry_op_ssha512_sum0r = fu_cry && s2_uop == CRY_SSHA512_SUM0R;
+wire        cry_op_ssha512_sum1r = fu_cry && s2_uop == CRY_SSHA512_SUM1R;
+wire        cry_op_ssha512_sig0l = fu_cry && s2_uop == CRY_SSHA512_SIG0L;
+wire        cry_op_ssha512_sig0h = fu_cry && s2_uop == CRY_SSHA512_SIG0H;
+wire        cry_op_ssha512_sig1l = fu_cry && s2_uop == CRY_SSHA512_SIG1L;
+wire        cry_op_ssha512_sig1h = fu_cry && s2_uop == CRY_SSHA512_SIG1H;
+wire        cry_op_ssm3_p0       = fu_cry && s2_uop == CRY_SSM3_P0      ;
+wire        cry_op_ssm3_p1       = fu_cry && s2_uop == CRY_SSM3_P1      ;
 
 wire        cry_ready            ; // Outputs ready.
 wire [XL:0] cry_rd               ;
@@ -320,7 +321,7 @@ riscv_crypto_fu #(
 .valid           (cry_valid           ), // Inputs valid.
 .rs1             (cry_rs1             ), // Source register 1
 .rs2             (cry_rs2             ), // Source register 2
-.imm             (cry_imm             ), // bs, enc_rcon for aes32/64.
+.imm             (cry_bs              ), // bs, enc_rcon for aes32/64.
 .op_lut4lo       (cry_op_lut4lo       ), // RV32 lut4-lo instruction
 .op_lut4hi       (cry_op_lut4hi       ), // RV32 lut4-hi instruction
 .op_saes32_encs  (cry_op_saes32_encs  ), // RV32 AES Encrypt SBox
@@ -357,7 +358,7 @@ wire [FU:0] n_s3_fu    = s2_fu   ; // Functional Unit
 wire [ 1:0] n_s3_size  = s2_size ; // Size of the instruction.
 wire [31:0] n_s3_instr = s2_instr; // The instruction word
 
-wire [ 4:0] n_s3_uop   = cfu_valid ? n_s3_uop_cfu : s2_uop  ; // Micro-op code
+wire [OP:0] n_s3_uop   = cfu_valid ? n_s3_uop_cfu : s2_uop  ; // Micro-op code
 
 wire        n_s3_trap  = s2_trap || 
                          fu_lsu && (lsu_a_error);

rtl/core/frv_common.svh
@@ -27,33 +27,33 @@ localparam P_FU_CSR     = 4;    // CSR accesses
 localparam P_FU_CRY     = 5;    // Crypto FU
 
 localparam FU           = 5;    // Width-1 of functional unit specifier field
-localparam OP           = 4;    // Width-1 of micro-op specifier field.
+localparam OP           = 5;    // Width-1 of micro-op specifier field.
 
-localparam ALU_ADD      = {2'b00, 3'b001};
-localparam ALU_SUB      = {2'b00, 3'b000};
-localparam ALU_AND      = {2'b01, 3'b001};
-localparam ALU_OR       = {2'b01, 3'b010};
-localparam ALU_XOR      = {2'b01, 3'b100};
-localparam ALU_SLT      = {2'b10, 3'b001};
-localparam ALU_SLTU     = {2'b10, 3'b010};
-localparam ALU_SRA      = {2'b11, 3'b001};
-localparam ALU_SRL      = {2'b11, 3'b010};
-localparam ALU_SLL      = {2'b11, 3'b100};
+localparam ALU_ADD      = {2'b00, 4'b001};
+localparam ALU_SUB      = {2'b00, 4'b000};
+localparam ALU_AND      = {2'b01, 4'b001};
+localparam ALU_OR       = {2'b01, 4'b010};
+localparam ALU_XOR      = {2'b01, 4'b100};
+localparam ALU_SLT      = {2'b10, 4'b001};
+localparam ALU_SLTU     = {2'b10, 4'b010};
+localparam ALU_SRA      = {2'b11, 4'b001};
+localparam ALU_SRL      = {2'b11, 4'b010};
+localparam ALU_SLL      = {2'b11, 4'b100};
 
-localparam CFU_BEQ      = {2'b00, 3'b001};
-localparam CFU_BGE      = {2'b00, 3'b010};
-localparam CFU_BGEU     = {2'b00, 3'b011};
-localparam CFU_BLT      = {2'b00, 3'b100};
-localparam CFU_BLTU     = {2'b00, 3'b101};
-localparam CFU_BNE      = {2'b00, 3'b110};
-localparam CFU_EBREAK   = {2'b01, 3'b001};
-localparam CFU_ECALL    = {2'b01, 3'b010};
-localparam CFU_MRET     = {2'b01, 3'b100};
-localparam CFU_JMP      = {2'b10, 3'b001};
-localparam CFU_JALI     = {2'b10, 3'b010};
-localparam CFU_JALR     = {2'b10, 3'b100};
-localparam CFU_TAKEN    = {2'b11, 3'b001};
-localparam CFU_NOT_TAKEN= {2'b11, 3'b000};
+localparam CFU_BEQ      = {2'b00, 4'b001};
+localparam CFU_BGE      = {2'b00, 4'b010};
+localparam CFU_BGEU     = {2'b00, 4'b011};
+localparam CFU_BLT      = {2'b00, 4'b100};
+localparam CFU_BLTU     = {2'b00, 4'b101};
+localparam CFU_BNE      = {2'b00, 4'b110};
+localparam CFU_EBREAK   = {2'b01, 4'b001};
+localparam CFU_ECALL    = {2'b01, 4'b010};
+localparam CFU_MRET     = {2'b01, 4'b100};
+localparam CFU_JMP      = {2'b10, 4'b001};
+localparam CFU_JALI     = {2'b10, 4'b010};
+localparam CFU_JALR     = {2'b10, 4'b100};
+localparam CFU_TAKEN    = {2'b11, 4'b001};
+localparam CFU_NOT_TAKEN= {2'b11, 4'b000};
 
 localparam LSU_SIGNED   = 0;
 localparam LSU_LOAD     = 3;
@@ -62,35 +62,33 @@ localparam LSU_BYTE     = 2'b01;
 localparam LSU_HALF     = 2'b10;
 localparam LSU_WORD     = 2'b11;
 
-localparam MUL_DIV      = {2'b11, 3'b000};
-localparam MUL_DIVU     = {2'b11, 3'b001};
-localparam MUL_REM      = {2'b11, 3'b100};
-localparam MUL_REMU     = {2'b11, 3'b101};
-localparam MUL_MUL      = {2'b01, 3'b100};
-localparam MUL_MULH     = {2'b01, 3'b110};
-localparam MUL_MULHSU   = {2'b01, 3'b111};
-localparam MUL_MULHU    = {2'b01, 3'b101};
+localparam MUL_DIV      = {2'b11, 4'b000};
+localparam MUL_DIVU     = {2'b11, 4'b001};
+localparam MUL_REM      = {2'b11, 4'b100};
+localparam MUL_REMU     = {2'b11, 4'b101};
+localparam MUL_MUL      = {2'b01, 4'b100};
+localparam MUL_MULH     = {2'b01, 4'b110};
+localparam MUL_MULHSU   = {2'b01, 4'b111};
+localparam MUL_MULHU    = {2'b01, 4'b101};
 
-localparam CRY_LUT4LO        = 5'b00_001; // lut4-lo instruction
-localparam CRY_LUT4HI        = 5'b00_010; // lut4-hi instruction
-localparam CRY_SAES32_ENCS   = 5'b01_000; // AES Encrypt SBox
-localparam CRY_SAES32_ENCSM  = 5'b01_001; // AES Encrypt SBox + MixCols
-localparam CRY_SAES32_DECS   = 5'b01_010; // AES Decrypt SBox
-localparam CRY_SAES32_DECSM  = 5'b01_011; // AES Decrypt SBox + MixCols
-localparam CRY_SSM3_P0       = 5'b01_100; // SSM3 P0
-localparam CRY_SSM3_P1       = 5'b01_101; // SSM3 P1
-localparam CRY_SSM4_KS       = 5'b01_110; // SSM4 KeySchedule
-localparam CRY_SSM4_ED       = 5'b01_111; // SSM4 Encrypt/Decrypt
-localparam CRY_SSHA256_SIG0  = 5'b10_000; // SHA256 Sigma 0
-localparam CRY_SSHA256_SIG1  = 5'b10_001; // SHA256 Sigma 1
-localparam CRY_SSHA256_SUM0  = 5'b10_010; // SHA256 Sum 0
-localparam CRY_SSHA256_SUM1  = 5'b10_011; // SHA256 Sum 1
-localparam CRY_SSHA512_SUM0R = 5'b11_000; // SHA512 Sum 0
-localparam CRY_SSHA512_SUM1R = 5'b11_001; // SHA512 Sum 1
-localparam CRY_SSHA512_SIG0L = 5'b11_010; // SHA512 Sigma 0 low
-localparam CRY_SSHA512_SIG0H = 5'b11_011; // SHA512 Sigma 0 high
-localparam CRY_SSHA512_SIG1L = 5'b11_100; // SHA512 Sigma 1 low
-localparam CRY_SSHA512_SIG1H = 5'b11_101; // SHA512 Sigma 1 high
+localparam CRY_SAES32_ENCS   = 6'b00_0000; // AES Encrypt SBox
+localparam CRY_SAES32_ENCSM  = 6'b00_0001; // AES Encrypt SBox + MixCols
+localparam CRY_SAES32_DECS   = 6'b00_0010; // AES Decrypt SBox
+localparam CRY_SAES32_DECSM  = 6'b00_0011; // AES Decrypt SBox + MixCols
+localparam CRY_SSM4_KS       = 6'b00_0100; // SSM4 KeySchedule
+localparam CRY_SSM4_ED       = 6'b00_0101; // SSM4 Encrypt/Decrypt
+localparam CRY_SSM3_P0       = 6'b00_1000; // SSM3 P0
+localparam CRY_SSM3_P1       = 6'b00_1001; // SSM3 P1
+localparam CRY_SSHA256_SIG0  = 6'b10_1000; // SHA256 Sigma 0
+localparam CRY_SSHA256_SIG1  = 6'b10_1001; // SHA256 Sigma 1
+localparam CRY_SSHA256_SUM0  = 6'b10_1010; // SHA256 Sum 0
+localparam CRY_SSHA256_SUM1  = 6'b10_1011; // SHA256 Sum 1
+localparam CRY_SSHA512_SUM0R = 6'b01_1000; // SHA512 Sum 0
+localparam CRY_SSHA512_SUM1R = 6'b01_1001; // SHA512 Sum 1
+localparam CRY_SSHA512_SIG0L = 6'b01_1010; // SHA512 Sigma 0 low
+localparam CRY_SSHA512_SIG0H = 6'b01_1011; // SHA512 Sigma 0 high
+localparam CRY_SSHA512_SIG1L = 6'b01_1100; // SHA512 Sigma 1 low
+localparam CRY_SSHA512_SIG1H = 6'b01_1101; // SHA512 Sigma 1 high
 
 localparam CSR_READ     = 4;
 localparam CSR_WRITE    = 3;

rtl/core/frv_pipeline_decode.sv
@@ -160,63 +160,63 @@ end endgenerate
 // -------------------------------------------------------------------------
 
 wire [OP:0] uop_alu = 
-    {5{dec_add       }} & ALU_ADD   |
-    {5{dec_addi      }} & ALU_ADD   |
-    {5{dec_c_add     }} & ALU_ADD   |
-    {5{dec_c_addi    }} & ALU_ADD   |
-    {5{dec_c_addi16sp}} & ALU_ADD   |
-    {5{dec_c_addi4spn}} & ALU_ADD   |
-    {5{dec_c_mv      }} & ALU_ADD   |
-    {5{dec_auipc     }} & ALU_ADD   |
-    {5{dec_c_sub     }} & ALU_SUB   |
-    {5{dec_sub       }} & ALU_SUB   |
-    {5{dec_and       }} & ALU_AND   |
-    {5{dec_andi      }} & ALU_AND   |
-    {5{dec_c_and     }} & ALU_AND   |
-    {5{dec_c_andi    }} & ALU_AND   |
-    {5{dec_lui       }} & ALU_OR    |
-    {5{dec_c_li      }} & ALU_OR    |
-    {5{dec_c_lui     }} & ALU_OR    |
-    {5{dec_c_nop     }} & ALU_OR    |
-    {5{dec_or        }} & ALU_OR    |
-    {5{dec_ori       }} & ALU_OR    |
-    {5{dec_c_or      }} & ALU_OR    |
-    {5{dec_c_xor     }} & ALU_XOR   |
-    {5{dec_xor       }} & ALU_XOR   |
-    {5{dec_xori      }} & ALU_XOR   |
-    {5{dec_slt       }} & ALU_SLT   |
-    {5{dec_slti      }} & ALU_SLT   |
-    {5{dec_sltu      }} & ALU_SLTU  |
-    {5{dec_sltiu     }} & ALU_SLTU  |
-    {5{dec_sra       }} & ALU_SRA   |
-    {5{dec_srai      }} & ALU_SRA   |
-    {5{dec_c_srai    }} & ALU_SRA   |
-    {5{dec_c_srli    }} & ALU_SRL   |
-    {5{dec_srl       }} & ALU_SRL   |
-    {5{dec_srli      }} & ALU_SRL   |
-    {5{dec_sll       }} & ALU_SLL   |
-    {5{dec_slli      }} & ALU_SLL   |
-    {5{dec_c_slli    }} & ALU_SLL   ;
+    {OP+1{dec_add       }} & ALU_ADD   |
+    {OP+1{dec_addi      }} & ALU_ADD   |
+    {OP+1{dec_c_add     }} & ALU_ADD   |
+    {OP+1{dec_c_addi    }} & ALU_ADD   |
+    {OP+1{dec_c_addi16sp}} & ALU_ADD   |
+    {OP+1{dec_c_addi4spn}} & ALU_ADD   |
+    {OP+1{dec_c_mv      }} & ALU_ADD   |
+    {OP+1{dec_auipc     }} & ALU_ADD   |
+    {OP+1{dec_c_sub     }} & ALU_SUB   |
+    {OP+1{dec_sub       }} & ALU_SUB   |
+    {OP+1{dec_and       }} & ALU_AND   |
+    {OP+1{dec_andi      }} & ALU_AND   |
+    {OP+1{dec_c_and     }} & ALU_AND   |
+    {OP+1{dec_c_andi    }} & ALU_AND   |
+    {OP+1{dec_lui       }} & ALU_OR    |
+    {OP+1{dec_c_li      }} & ALU_OR    |
+    {OP+1{dec_c_lui     }} & ALU_OR    |
+    {OP+1{dec_c_nop     }} & ALU_OR    |
+    {OP+1{dec_or        }} & ALU_OR    |
+    {OP+1{dec_ori       }} & ALU_OR    |
+    {OP+1{dec_c_or      }} & ALU_OR    |
+    {OP+1{dec_c_xor     }} & ALU_XOR   |
+    {OP+1{dec_xor       }} & ALU_XOR   |
+    {OP+1{dec_xori      }} & ALU_XOR   |
+    {OP+1{dec_slt       }} & ALU_SLT   |
+    {OP+1{dec_slti      }} & ALU_SLT   |
+    {OP+1{dec_sltu      }} & ALU_SLTU  |
+    {OP+1{dec_sltiu     }} & ALU_SLTU  |
+    {OP+1{dec_sra       }} & ALU_SRA   |
+    {OP+1{dec_srai      }} & ALU_SRA   |
+    {OP+1{dec_c_srai    }} & ALU_SRA   |
+    {OP+1{dec_c_srli    }} & ALU_SRL   |
+    {OP+1{dec_srl       }} & ALU_SRL   |
+    {OP+1{dec_srli      }} & ALU_SRL   |
+    {OP+1{dec_sll       }} & ALU_SLL   |
+    {OP+1{dec_slli      }} & ALU_SLL   |
+    {OP+1{dec_c_slli    }} & ALU_SLL   ;
 
 wire [OP:0] uop_cfu =
-    {5{dec_beq       }} & CFU_BEQ   |
-    {5{dec_c_beqz    }} & CFU_BEQ   |
-    {5{dec_bge       }} & CFU_BGE   |
-    {5{dec_bgeu      }} & CFU_BGEU  |
-    {5{dec_blt       }} & CFU_BLT   |
-    {5{dec_bltu      }} & CFU_BLTU  |
-    {5{dec_bne       }} & CFU_BNE   |
-    {5{dec_c_bnez    }} & CFU_BNE   |
-    {5{dec_c_ebreak  }} & CFU_EBREAK|
-    {5{dec_ebreak    }} & CFU_EBREAK|
-    {5{dec_ecall     }} & CFU_ECALL |
-    {5{dec_c_j       }} & CFU_JALI  |
-    {5{dec_c_jr      }} & CFU_JALR  |
-    {5{dec_c_jal     }} & CFU_JALI  |
-    {5{dec_jal       }} & CFU_JALI  |
-    {5{dec_c_jalr    }} & CFU_JALR  |
-    {5{dec_jalr      }} & CFU_JALR  |
-    {5{dec_mret      }} & CFU_MRET  ;
+    {OP+1{dec_beq       }} & CFU_BEQ   |
+    {OP+1{dec_c_beqz    }} & CFU_BEQ   |
+    {OP+1{dec_bge       }} & CFU_BGE   |
+    {OP+1{dec_bgeu      }} & CFU_BGEU  |
+    {OP+1{dec_blt       }} & CFU_BLT   |
+    {OP+1{dec_bltu      }} & CFU_BLTU  |
+    {OP+1{dec_bne       }} & CFU_BNE   |
+    {OP+1{dec_c_bnez    }} & CFU_BNE   |
+    {OP+1{dec_c_ebreak  }} & CFU_EBREAK|
+    {OP+1{dec_ebreak    }} & CFU_EBREAK|
+    {OP+1{dec_ecall     }} & CFU_ECALL |
+    {OP+1{dec_c_j       }} & CFU_JALI  |
+    {OP+1{dec_c_jr      }} & CFU_JALR  |
+    {OP+1{dec_c_jal     }} & CFU_JALI  |
+    {OP+1{dec_jal       }} & CFU_JALI  |
+    {OP+1{dec_c_jalr    }} & CFU_JALR  |
+    {OP+1{dec_jalr      }} & CFU_JALR  |
+    {OP+1{dec_mret      }} & CFU_MRET  ;
 
 wire [1:0] lsu_width = 
     {2{dec_lb        }} & LSU_BYTE |
@@ -233,6 +233,8 @@ wire [1:0] lsu_width =
     {2{dec_sw        }} & LSU_WORD ;
 
 wire [OP:0] uop_lsu;
+
+assign uop_lsu[OP ]      = 1'b0;
 
 assign uop_lsu[2:1]      = lsu_width;
 
@@ -257,35 +259,36 @@ assign uop_lsu[LSU_SIGNED] =
     dec_lh          ;
 
 wire [OP:0] uop_mul = 
-    {5{dec_div          }} & MUL_DIV    |
-    {5{dec_divu         }} & MUL_DIVU   |
-    {5{dec_rem          }} & MUL_REM    |
-    {5{dec_remu         }} & MUL_REMU   |
-    {5{dec_mul          }} & MUL_MUL    |
-    {5{dec_mulh         }} & MUL_MULH   |
-    {5{dec_mulhsu       }} & MUL_MULHSU |
-    {5{dec_mulhu        }} & MUL_MULHU  ;
+    {OP+1{dec_div          }} & MUL_DIV    |
+    {OP+1{dec_divu         }} & MUL_DIVU   |
+    {OP+1{dec_rem          }} & MUL_REM    |
+    {OP+1{dec_remu         }} & MUL_REMU   |
+    {OP+1{dec_mul          }} & MUL_MUL    |
+    {OP+1{dec_mulh         }} & MUL_MULH   |
+    {OP+1{dec_mulhsu       }} & MUL_MULHSU |
+    {OP+1{dec_mulhu        }} & MUL_MULHU  ;
 
+wire [OP:0] uop_cry_bs  = {d_data[31:30], {OP-1{1'b0}}};
 
-wire [OP:0] uop_cry = 
-    {5{dec_saes32_encs  }} & CRY_SAES32_ENCS   |
-    {5{dec_saes32_encsm }} & CRY_SAES32_ENCSM  |
-    {5{dec_saes32_decs  }} & CRY_SAES32_DECS   |
-    {5{dec_saes32_decsm }} & CRY_SAES32_DECSM  |
-    {5{dec_ssha256_sig0 }} & CRY_SSHA256_SIG0  |
-    {5{dec_ssha256_sig1 }} & CRY_SSHA256_SIG1  |
-    {5{dec_ssha256_sum0 }} & CRY_SSHA256_SUM0  |
-    {5{dec_ssha256_sum1 }} & CRY_SSHA256_SUM1  |
-    {5{dec_ssha512_sum0r}} & CRY_SSHA512_SUM0R |
-    {5{dec_ssha512_sum1r}} & CRY_SSHA512_SUM1R |
-    {5{dec_ssha512_sig0l}} & CRY_SSHA512_SIG0L |
-    {5{dec_ssha512_sig0h}} & CRY_SSHA512_SIG0H |
-    {5{dec_ssha512_sig1l}} & CRY_SSHA512_SIG1L |
-    {5{dec_ssha512_sig1h}} & CRY_SSHA512_SIG1H |
-    {5{dec_ssm3_p0      }} & CRY_SSM3_P0       |
-    {5{dec_ssm3_p1      }} & CRY_SSM3_P1       |
-    {5{dec_ssm4_ks      }} & CRY_SSM4_KS       |
-    {5{dec_ssm4_ed      }} & CRY_SSM4_ED       ;
+wire [OP:0] uop_cry     = 
+    {OP+1{dec_saes32_encs  }} & (CRY_SAES32_ENCS   | uop_cry_bs) |
+    {OP+1{dec_saes32_encsm }} & (CRY_SAES32_ENCSM  | uop_cry_bs) |
+    {OP+1{dec_saes32_decs  }} & (CRY_SAES32_DECS   | uop_cry_bs) |
+    {OP+1{dec_saes32_decsm }} & (CRY_SAES32_DECSM  | uop_cry_bs) |
+    {OP+1{dec_ssm4_ks      }} & (CRY_SSM4_KS       | uop_cry_bs) |
+    {OP+1{dec_ssm4_ed      }} & (CRY_SSM4_ED       | uop_cry_bs) |
+    {OP+1{dec_ssha256_sig0 }} & (CRY_SSHA256_SIG0              ) |
+    {OP+1{dec_ssha256_sig1 }} & (CRY_SSHA256_SIG1              ) |
+    {OP+1{dec_ssha256_sum0 }} & (CRY_SSHA256_SUM0              ) |
+    {OP+1{dec_ssha256_sum1 }} & (CRY_SSHA256_SUM1              ) |
+    {OP+1{dec_ssha512_sum0r}} & (CRY_SSHA512_SUM0R             ) |
+    {OP+1{dec_ssha512_sum1r}} & (CRY_SSHA512_SUM1R             ) |
+    {OP+1{dec_ssha512_sig0l}} & (CRY_SSHA512_SIG0L             ) |
+    {OP+1{dec_ssha512_sig0h}} & (CRY_SSHA512_SIG0H             ) |
+    {OP+1{dec_ssha512_sig1l}} & (CRY_SSHA512_SIG1L             ) |
+    {OP+1{dec_ssha512_sig1h}} & (CRY_SSHA512_SIG1H             ) |
+    {OP+1{dec_ssm3_p0      }} & (CRY_SSM3_P0                   ) |
+    {OP+1{dec_ssm3_p1      }} & (CRY_SSM3_P1                   ) ;
 
 wire [OP:0] uop_csr;
 
@@ -298,6 +301,7 @@ wire csr_no_write = ((dec_csrrs  || dec_csrrc ) && dec_rs1_32 == 0) ||
 
 wire csr_no_read  = (dec_csrrw || dec_csrrwi) && dec_rd_32 == 0;
 
+assign uop_csr[OP       ] = 1'b0;
 assign uop_csr[CSR_READ ] = csr_op && !csr_no_read ;
 assign uop_csr[CSR_WRITE] = csr_op && !csr_no_write;
 assign uop_csr[CSR_SET  ] = dec_csrrs || dec_csrrsi ;

rtl/core/frv_pipeline_decode.svh
@@ -90,13 +90,6 @@ wire dec_saes32_encsm   = (d_data & 32'h3e00707f) == 32'h202b;
 wire dec_saes32_encs    = (d_data & 32'h3e00707f) == 32'h200202b;
 wire dec_saes32_decsm   = (d_data & 32'h3e00707f) == 32'h400202b;
 wire dec_saes32_decs    = (d_data & 32'h3e00707f) == 32'h600202b;
-wire dec_saes64_ks1     = (d_data & 32'hff00707f) == 32'h800202b;
-wire dec_saes64_ks2     = (d_data & 32'hfe00707f) == 32'ha00202b;
-wire dec_saes64_imix    = (d_data & 32'hfff0707f) == 32'hc10202b;
-wire dec_saes64_encsm   = (d_data & 32'hfe00707f) == 32'he00202b;
-wire dec_saes64_encs    = (d_data & 32'hfe00707f) == 32'h1000202b;
-wire dec_saes64_decsm   = (d_data & 32'hfe00707f) == 32'h1200202b;
-wire dec_saes64_decs    = (d_data & 32'hfe00707f) == 32'h1400202b;
 wire dec_ssha256_sig0   = (d_data & 32'hfff0707f) == 32'he00702b;
 wire dec_ssha256_sig1   = (d_data & 32'hfff0707f) == 32'he10702b;
 wire dec_ssha256_sum0   = (d_data & 32'hfff0707f) == 32'he20702b;
@@ -109,10 +102,6 @@ wire dec_ssha512_sig1l  = (d_data & 32'hfe00707f) == 32'h1400702b;
 wire dec_ssha512_sig1h  = (d_data & 32'hfe00707f) == 32'h1600702b;
 wire dec_ssha512_sum0r  = (d_data & 32'hfe00707f) == 32'h1800702b;
 wire dec_ssha512_sum1r  = (d_data & 32'hfe00707f) == 32'h1a00702b;
-wire dec_ssha512_sig0   = (d_data & 32'hfff0707f) == 32'he40702b;
-wire dec_ssha512_sig1   = (d_data & 32'hfff0707f) == 32'he50702b;
-wire dec_ssha512_sum0   = (d_data & 32'hfff0707f) == 32'he60702b;
-wire dec_ssha512_sum1   = (d_data & 32'hfff0707f) == 32'he70702b;
 wire dec_pollentropy    = (d_data & 32'hfe0ff07f) == 32'he05702b;
 
 

rtl/core/frv_pipeline_memory.sv
@@ -109,6 +109,7 @@ wire fu_mul = s3_fu[P_FU_MUL];
 wire fu_lsu = s3_fu[P_FU_LSU];
 wire fu_cfu = s3_fu[P_FU_CFU];
 wire fu_csr = s3_fu[P_FU_CSR];
+wire fu_cry = s3_fu[P_FU_CRY];
 
 //
 // Functional Unit Interfacing: LSU
@@ -151,7 +152,7 @@ wire[4:0] n_s4_rd     =     s3_trap  ? s3_rd         :
 // -------------------------------------------------------------------------
 
 wire opra_ld_en = instr_out && (
-    fu_alu || fu_mul || fu_lsu || fu_cfu || fu_csr
+    fu_alu || fu_mul || fu_lsu || fu_cfu || fu_csr || fu_cry
 ); 
 
 wire oprb_ld_en = instr_out && (

rtl/core/frv_pipeline_writeback.sv
@@ -153,6 +153,7 @@ wire fu_mul = s4_fu[P_FU_MUL];
 wire fu_lsu = s4_fu[P_FU_LSU];
 wire fu_cfu = s4_fu[P_FU_CFU];
 wire fu_csr = s4_fu[P_FU_CSR];
+wire fu_cry = s4_fu[P_FU_CRY];
 
 //
 // Functional Unit: ALU
@@ -167,6 +168,13 @@ wire [XL:0] alu_gpr_wdata   = s4_opr_a;
 
 wire        mul_gpr_wen     = fu_mul;
 wire [XL:0] mul_gpr_wdata   = s4_opr_a;
+
+//
+// Functional Unit: Crypto
+// -------------------------------------------------------------------------
+
+wire        cry_gpr_wen     = fu_cry;
+wire [XL:0] cry_gpr_wdata   = s4_opr_a;
 
 //
 // Functional Unit: CSR
@@ -359,13 +367,14 @@ assign gpr_rd   = s4_rd;
 
 assign gpr_wen  = !s4_trap && !trap_int && !gpr_write_done &&
     (csr_gpr_wen || alu_gpr_wen || lsu_gpr_wen ||
-     cfu_gpr_wen || mul_gpr_wen );
+     cfu_gpr_wen || mul_gpr_wen || cry_gpr_wen );
 
 assign gpr_wdata= {32{csr_gpr_wen}} & csr_gpr_wdata |
                   {32{alu_gpr_wen}} & alu_gpr_wdata |
                   {32{lsu_gpr_wen}} & lsu_gpr_wdata |
                   {32{cfu_gpr_wen}} & cfu_gpr_wdata |
-                  {32{mul_gpr_wen}} & mul_gpr_wdata ;
+                  {32{mul_gpr_wen}} & mul_gpr_wdata |
+                  {32{cry_gpr_wen}} & cry_gpr_wdata ;
 
 assign fwd_s4_rd    = |s4_size ? gpr_rd : 5'b0;
 assign fwd_s4_wdata = gpr_wdata;

commit 4548c02deca909b52767326387b5b3fd38460473

skw: rtl: Fix broken CFU condition extraction in EX.
- Change in aa92755 introduced the bug.

- Due to change in width of the *uop pipeline field.

- Caused riscv-compliance to fail.

 On branch scarv/skywater/dev
 Your branch is up-to-date with 'origin/scarv/skywater/dev'.

 Changes to be committed:
	modified:   rtl/core/frv_pipeline_execute.sv

 Changes not staged for commit:
	modified:   external/embench-iot (modified content, untracked content)
	modified:   external/riscv-formal (untracked content)

rtl/core/frv_pipeline_execute.sv
@@ -150,8 +150,8 @@ wire [XL:0] n_s3_opr_b_lsu = s2_opr_c       ;
 wire        cfu_valid   = fu_cfu        ; // Inputs are valid.
 wire        cfu_ready   = cfu_valid     ; // Instruction complete.
 
-wire        cfu_cond    = cfu_valid && s2_uop[4:3] == 2'b00;
-wire        cfu_uncond  = cfu_valid && s2_uop[4:3] == 2'b10;
+wire        cfu_cond    = cfu_valid && s2_uop[OP:OP-1] == 2'b00;
+wire        cfu_uncond  = cfu_valid && s2_uop[OP:OP-1] == 2'b10;
 wire        cfu_jmp     = cfu_valid && s2_uop      == CFU_JMP ;
 wire        cfu_jali    = cfu_valid && s2_uop      == CFU_JALI;
 wire        cfu_jalr    = cfu_valid && s2_uop      == CFU_JALR;