730abecccd57e4a42e488eefe8072fd0757145ef
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-08-10 16:07:07 +0100

    skw: src/csp: Change access to linker provided variables.
     On branch scarv/skywater/dev
     Your branch is up-to-date with 'origin/scarv/skywater/dev'.
    
     Changes to be committed:
    	modified:   src/csp/scarv_cpu_csp.c
    	modified:   src/csp/scarv_cpu_csp.h
    
     Changes not staged for commit:
    	modified:   .travis.yml
    	modified:   external/embench-iot (modified content, untracked content)
    	modified:   external/riscv-formal (untracked content)
    	modified:   rtl/ccx/scarv_ccx_mmio.sv
    	modified:   verif/unit/Makefile.in
    	modified:   verif/unit/ccx-counters/Makefile.in
    	modified:   verif/unit/ccx-counters/test_counters.c
    	modified:   verif/unit/share/core-link.ld
    	modified:   verif/unit/share/unit_test.c
    	modified:   verif/unit/share/unit_test.h
    	modified:   verif/unit/share/util.S

--- src/csp/scarv_cpu_csp.c
+++ src/csp/scarv_cpu_csp.c
@@ -1,11 +1,13 @@
 
 #include "scarv_cpu_csp.h"
 
-extern uint32_t volatile * const __mmio_mtime_lo   ;
-extern uint32_t volatile * const __mmio_mtime_hi   ;
-extern uint32_t volatile * const __mmio_mtimecmp_lo;
-extern uint32_t volatile * const __mmio_mtimecmp_hi;
-extern uint32_t volatile * const __mmio_trng       ;
+//
+// These are all provided by the linker script.
+extern uint32_t volatile * __mmio_mtime_lo   ;
+extern uint32_t volatile * __mmio_mtime_hi   ;
+extern uint32_t volatile * __mmio_mtimecmp_lo;
+extern uint32_t volatile * __mmio_mtimecmp_hi;
+extern uint32_t volatile * __mmio_trng       ;
 
 /*!
 @details
@@ -19,9 +21,9 @@ const uint32_t scarv_cpu_freq = 50000000;
 inline uint64_t scarv_cpu_get_mtime(){
     uint32_t lo1, lo2, hi1;
     do {
-        lo1 = __mmio_mtime_lo[0];
-        hi1 = __mmio_mtime_lo[1];
-        lo2 = __mmio_mtime_lo[0];
+        lo1 = (uint32_t)(&__mmio_mtime_lo)[0];
+        hi1 = (uint32_t)(&__mmio_mtime_lo)[1];
+        lo2 = (uint32_t)(&__mmio_mtime_lo)[0];
     } while(lo2 < lo1);
     return ((uint64_t)hi1 << 32) | lo2;
 }
@@ -29,21 +31,21 @@ inline uint64_t scarv_cpu_get_mtime(){
 /*!
 */
 inline uint32_t scarv_cpu_get_mtime_lo(){
-    return *__mmio_mtime_lo;
+    return (uint32_t)(&__mmio_mtime_lo)[0];
 }
 
 /*!
 */
 inline uint64_t scarv_cpu_get_mtimecmp(){
     uint32_t lo1, hi1;
-    lo1 = __mmio_mtimecmp_lo[0];
-    hi1 = __mmio_mtimecmp_lo[1];
+    lo1 = (uint32_t)(&__mmio_mtimecmp_lo)[0];
+    hi1 = (uint32_t)(&__mmio_mtimecmp_lo)[1];
     return ((uint64_t)hi1 << 32) | lo1;
 }
 
 /*!
 */
 inline void     scarv_cpu_set_mtimecmp(uint64_t nv){
-    *__mmio_mtimecmp_hi = (uint32_t)(nv >> 32);
-    *__mmio_mtimecmp_lo = (uint32_t)(nv >>  0);
+    (__mmio_mtimecmp_lo)[0] = (uint32_t)(nv >> 32);
+    (__mmio_mtimecmp_lo)[1] = (uint32_t)(nv >>  0);
 }
--- src/csp/scarv_cpu_csp.h
+++ src/csp/scarv_cpu_csp.h
@@ -46,6 +46,28 @@ inline void     scarv_cpu_wait_for_interrupt() {
     asm volatile ("wfi");
 }
 
+//! Read the current cycle count
+inline uint64_t scarv_cpu_rdcycle() {
+    uint32_t lo1, lo2, hi1;
+    do {
+        asm volatile ("rdcycle  %0" : "=r"(lo1) : );
+        asm volatile ("rdcycleh %0" : "=r"(hi1) : );
+        asm volatile ("rdcycle  %0" : "=r"(lo2) : );
+    } while(lo2 < lo1);
+    return ((uint64_t)hi1 << 32) | lo2;
+}
+
+//! Read the current instructions retired count
+inline uint64_t scarv_cpu_rdinstret() {
+    uint32_t lo1, lo2, hi1;
+    do {
+        asm volatile ("rdinstret  %0" : "=r"(lo1) : );
+        asm volatile ("rdinstreth %0" : "=r"(hi1) : );
+        asm volatile ("rdinstret  %0" : "=r"(lo2) : );
+    } while(lo2 < lo1);
+    return ((uint64_t)hi1 << 32) | lo2;
+}
+
 // ----------- CSR Constant Codes ------------------
 
 #define SCARV_CPU_MCAUSE_IALIGN         (0x00000000)
@@ -63,47 +85,46 @@ inline void     scarv_cpu_wait_for_interrupt() {
 
 
 //! Declare a volatile inline function for Reading a CSR value
-#define DECL_RD_CSR(CSR) volatile inline uint32_t scarv_cpu_get_##CSR() { \
-    uint32_t rd; asm volatile ("csrr %0, " #CSR : "=r"(rd)); return rd; \
+#define DECL_RD_CSR(CSR,A) volatile inline uint32_t scarv_cpu_get_##CSR() { \
+    uint32_t rd; asm volatile ("csrr %0, " #A : "=r"(rd)); return rd; \
 }
 
 //! Declare a volatile inline function for Writing a CSR value
-#define DECL_WR_CSR(CSR) volatile inline void scarv_cpu_wr_##CSR(uint32_t rs1) { \
-    asm volatile ("csrw " #CSR ", %0" : : "r"(rs1));   \
+#define DECL_WR_CSR(CSR,A) volatile inline void scarv_cpu_wr_##CSR(uint32_t rs1) { \
+    asm volatile ("csrw " #A ", %0" : : "r"(rs1));   \
 }
 
 //! Declare a volatile inline function for Clearing CSR bits.
-#define DECL_CLR_CSR(CSR) volatile inline void scarv_cpu_clr_##CSR(uint32_t rs1) { \
-    asm volatile ("csrc " #CSR ", %0" : : "r"(rs1));   \
+#define DECL_CLR_CSR(CSR,A) volatile inline void scarv_cpu_clr_##CSR(uint32_t rs1) { \
+    asm volatile ("csrc " #A ", %0" : : "r"(rs1));   \
 }
 
 //! Declare a volatile inline function for Setting CSR bits.
-#define DECL_SET_CSR(CSR) volatile inline void scarv_cpu_set_##CSR(uint32_t rs1) { \
-    asm volatile ("csrs " #CSR ", %0" : : "r"(rs1));   \
+#define DECL_SET_CSR(CSR,A) volatile inline void scarv_cpu_set_##CSR(uint32_t rs1) { \
+    asm volatile ("csrs " #A ", %0" : : "r"(rs1));   \
 }
 
 //! Top level macro for defining all CSR accessor functions.
-#define DECL_CSR_ACCESS(CSR) \
-    DECL_RD_CSR(CSR)         \
-    DECL_WR_CSR(CSR)         \
-    DECL_CLR_CSR(CSR)        \
-    DECL_SET_CSR(CSR) 
+#define DECL_CSR_ACCESS(CSR,A) \
+    DECL_RD_CSR(CSR,A)         \
+    DECL_WR_CSR(CSR,A)         \
+    DECL_CLR_CSR(CSR,A)        \
+    DECL_SET_CSR(CSR,A) 
 
-DECL_RD_CSR(misa)
-DECL_RD_CSR(mvendorid)
-DECL_RD_CSR(marchid)
-DECL_RD_CSR(mimpid)
-
-DECL_CSR_ACCESS(mepc)
-DECL_CSR_ACCESS(mcause)
-DECL_CSR_ACCESS(mtvec)
-DECL_CSR_ACCESS(mtval)
-DECL_CSR_ACCESS(mstatus)
-DECL_CSR_ACCESS(mie)
-DECL_CSR_ACCESS(mip)
-DECL_CSR_ACCESS(mcounteren)
-DECL_CSR_ACCESS(mcountinhibit)
-DECL_CSR_ACCESS(mscratch)
+DECL_RD_CSR(misa,0x301)
+DECL_RD_CSR(mvendorid,0xF11)
+DECL_RD_CSR(marchid,0xF12)
+DECL_RD_CSR(mimpid,0xF13)
+DECL_CSR_ACCESS(mepc,0x341)
+DECL_CSR_ACCESS(mcause,0x342)
+DECL_CSR_ACCESS(mtvec,0x305)
+DECL_CSR_ACCESS(mtval,0x343)
+DECL_CSR_ACCESS(mstatus,0x300)
+DECL_CSR_ACCESS(mie,0x304)
+DECL_CSR_ACCESS(mip,0x344)
+DECL_CSR_ACCESS(mcounteren,0x306)
+DECL_CSR_ACCESS(mcountinhibit,0x320)
+DECL_CSR_ACCESS(mscratch,0x340)
 
 #endif
 
