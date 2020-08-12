04fb9dc519984ecb25d15161a8b39fece7dd1377
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-08-12 13:38:30 +0100

    skw: verif: Formal checkers for sext.*. See #40
     On branch scarv/skywater/dev
     Your branch is ahead of 'origin/scarv/skywater/dev' by 6 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   flow/designer-assertions/insn_sext.sby
    	new file:   verif/designer-assertions/insn_checkers/insn_sext_checker.sv
    
     Changes not staged for commit:
    	modified:   external/embench-iot (modified content, untracked content)
    	modified:   external/riscv-formal (untracked content)
    	modified:   rtl/core/frv_alu.sv

---
+++ verif/designer-assertions/insn_checkers/insn_sext_checker.sv
@@ -0,0 +1,67 @@
+`include "defines.svh"
+`include "rvfi_macros.vh"
+`include "checkers_common.svh"
+
+//
+// module: insn_sext_checker
+//
+//  Checker for the Bitmanip sext[b|h] instructions.
+//
+module insn_sext_checker (
+input                                 rvfi_valid,
+input  [`RISCV_FORMAL_ILEN   - 1 : 0] rvfi_insn,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_pc_rdata,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs1_rdata,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs2_rdata,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_mem_rdata,
+`ifdef RISCV_FORMAL_CSR_MISA
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_csr_misa_rdata,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_csr_misa_rmask,
+`endif
+output                                spec_valid,
+output                                spec_trap,
+output [                       4 : 0] spec_rs1_addr,
+output [                       4 : 0] spec_rs2_addr,
+output [                       4 : 0] spec_rd_addr,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_rd_wdata,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_pc_wdata,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_mem_addr,
+output [`RISCV_FORMAL_XLEN/8 - 1 : 0] spec_mem_rmask,
+output [`RISCV_FORMAL_XLEN/8 - 1 : 0] spec_mem_wmask,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_mem_wdata
+);
+
+//
+// Macro is a 2-bit one-hot signal, which masks the individual instrucitons
+// which are checked when the design is run through pre-proof synthesis.
+// This is just an easy way to control the proof from symbiyosys without
+// duplicating lots of files per instruction.
+wire [1:0] check_en = `INSN_SEXT_CHECKER_EN;
+
+wire dec_sextb      = rvfi_valid && (rvfi_insn & 32'hfff0707f) == 32'h60401013;
+wire dec_sexth      = rvfi_valid && (rvfi_insn & 32'hfff0707f) == 32'h60501013;
+
+assign spec_valid   = 
+    dec_sextb   && check_en[0]  ||
+    dec_sexth   && check_en[1]  ;
+
+wire [31:0] result_sextb    = {{24{rvfi_rs1_rdata[ 7]}},rvfi_rs1_rdata[ 7:0]};
+wire [31:0] result_sexth    = {{16{rvfi_rs1_rdata[15]}},rvfi_rs1_rdata[15:0]};
+
+wire [31:0] result = 
+    {32{dec_sextb}} & result_sextb  |
+    {32{dec_sexth}} & result_sexth  ;
+
+// These instructions never trap.
+assign spec_trap        = 1'b0   ;
+
+assign spec_rs1_addr    = rvfi_insn[19:15];
+assign spec_rs2_addr    = 5'b0            ;
+assign spec_rd_addr     = rvfi_insn[11: 7];
+assign spec_rd_wdata    = |spec_rd_addr ? result : 32'b0;
+assign spec_pc_wdata    = rvfi_pc_rdata + 32'd4;
+assign spec_mem_rmask   = 32'b0;
+assign spec_mem_wmask   = 32'b0;
+assign spec_mem_wdata   = 32'b0;
+
+endmodule
