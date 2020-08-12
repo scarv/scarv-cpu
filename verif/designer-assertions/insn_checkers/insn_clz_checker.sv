44ec1b4a92d347a6d7399f0cb5431f20d813106c
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-08-12 14:20:55 +0100

    skw: verif: checkers for count leading/trailing zeros.
    - Closes #40
    
     On branch scarv/skywater/dev
     Your branch is ahead of 'origin/scarv/skywater/dev' by 11 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   flow/designer-assertions/insn_clz.sby
    	new file:   flow/designer-assertions/insn_ctz.sby
    	new file:   verif/designer-assertions/insn_checkers/insn_clz_checker.sv
    	new file:   verif/designer-assertions/insn_checkers/insn_ctz_checker.sv
    
     Changes not staged for commit:
    	modified:   external/embench-iot (modified content, untracked content)
    	modified:   external/riscv-formal (untracked content)

---
+++ verif/designer-assertions/insn_checkers/insn_clz_checker.sv
@@ -0,0 +1,68 @@
+`include "defines.svh"
+`include "rvfi_macros.vh"
+`include "checkers_common.svh"
+
+//
+// module: insn_clz_checker
+//
+//  Checker for the scalar clz (Count Leading Zeros) instructions.
+//
+module insn_clz_checker (
+input                                 rvfi_valid,
+input  [`RISCV_FORMAL_ILEN   - 1 : 0] rvfi_insn,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_pc_rdata,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs1_rdata,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs2_rdata,
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_mem_rdata,
+`ifdef RISCV_FORMAL_CSR_MISA
+input  [`RISCV_FORMAL_XLEN   - 1 : 0] rvfi_csr_misa_rdata,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_csr_misa_rmask,
+`endif
+output                                spec_valid,
+output                                spec_trap,
+output [                       4 : 0] spec_rs1_addr,
+output [                       4 : 0] spec_rs2_addr,
+output [                       4 : 0] spec_rd_addr,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_rd_wdata,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_pc_wdata,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_mem_addr,
+output [`RISCV_FORMAL_XLEN/8 - 1 : 0] spec_mem_rmask,
+output [`RISCV_FORMAL_XLEN/8 - 1 : 0] spec_mem_wmask,
+output [`RISCV_FORMAL_XLEN   - 1 : 0] spec_mem_wdata
+);
+
+wire dec_clz       = rvfi_valid && (rvfi_insn & 32'hfff0707f) == 32'h60001013;
+
+assign spec_valid   = dec_clz;
+
+reg  [31:0] result  ;
+reg         stop;
+
+integer i;
+always @(*) begin
+    result = 0;
+    stop   = 0;
+    if(spec_valid) begin
+        for(i = 31; i >= 0; i = i - 1) begin
+            if(32'b1 & (rvfi_rs1_rdata >> i)) begin
+                stop = 1;
+            end else if(!stop) begin
+                result = result + 1;
+            end
+        end
+    end
+end
+
+// These instructions never trap.
+assign spec_trap        = 1'b0   ;
+
+assign spec_rs1_addr    = rvfi_insn[19:15];
+assign spec_rs2_addr    = 5'b0;
+assign spec_rd_addr     = rvfi_insn[11: 7];
+assign spec_rd_wdata    = |spec_rd_addr ? result : 32'b0;
+assign spec_pc_wdata    = rvfi_pc_rdata + 32'd4;
+assign spec_mem_rmask   = 32'b0;
+assign spec_mem_wmask   = 32'b0;
+assign spec_mem_wdata   = 32'b0;
+
+endmodule
